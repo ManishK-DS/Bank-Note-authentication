4dbb13055322795303f5ba359073659b401f9f57
Manish kumar thota <37763863+ManishK-DS@users.noreply.github.com>
2020-07-06 16:08:47 +0530

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -2,8 +2,8 @@ from flask import Flask, request
 import numpy as np
 import pickle
 import pandas as pd
-import flasgger
-from flasgger import Swagger
+#import flasgger
+#from flasgger import Swagger
 
 app=Flask(__name__)
 Swagger(app)
